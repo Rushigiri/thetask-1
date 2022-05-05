7d18f1ac50d4dfbb8f83c99405d4d66f9b2c61b5
Rushi <rushigiri.gosai@yudiz.com>
2022-05-05 11:47:02 +0530

    submit

--- new 1/Assets/scripts/move.cs
+++ new 1/Assets/scripts/move.cs
@@ -4,35 +4,22 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
-    // Transforms to act as start and end markers for the journey.
-    //public Transform c1;
-    //public Transform c2;
+
     public List<Transform> c;
     int i = 0;
-
-    // Movement speed in units per second.
-   float speed = 1.0F;
-
-    // Time when the movement started.
-    private float startTime;
-
-    // Total distance between the markers.
-    private float journeyLength;
     public float spped1;
 
     void Start()
     {
-        //startTime = Time.time;
 
-       //journeyLength = Vector3.Distance(c[0].position, c[1].position);
     }
-           void Update()
+    void Update()
     {
         if (Input.GetMouseButton(0))
         {
-  
-            transform.position = Vector3.Lerp(transform.position, c[i].position,spped1*Time.deltaTime);
-            if(transform.position == c[i].position)
+
+            transform.position = Vector3.Lerp(transform.position, c[i].position, spped1 * Time.deltaTime);
+            if (transform.position == c[i].position)
             {
                 i++;
                 if (i == c.Count)
@@ -41,6 +28,6 @@ public class move : MonoBehaviour
                 }
             }
         }
-        
+
     }
 }
